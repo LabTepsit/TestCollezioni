9572c35fe44c9f5e40058f1561e1e9b212f92773
Valentina Falucca <valentina.falucca@hotmail.com>
2018-03-22 17:02:00 +0100

    Test competato

--- CollezioneLibrary.Test/VettoreTests.cs
+++ CollezioneLibrary.Test/VettoreTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CollezioneLibrary;
 
 namespace CollezioneLibrary.Test
 {
@@ -23,6 +24,26 @@ namespace CollezioneLibrary.Test
             int[] v2 = v1;
             bool sonouguali = (v1 == v2);
             Assert.AreEqual(true, sonouguali);
+
+        }
+
+        [TestMethod]
+        public void TestArrayContenuto()
+        {
+            int[] v1 = new int[] { 1, 2, 3, 4 };
+            int[] v2 = new int[] { 1, 2, 3, 4 };
+            
+            CollectionAssert.AreEqual(v1, v2);
+
+        }
+
+        [TestMethod]
+        public void TestArrayOrdinamento()
+        {
+            int[] v1 = new int[] { 11, 22, 13, 4 };
+            int[] v2 = new int[] { 4,11,13,22};
+            int[] vettoreOrdinato = Vettore.Ordina(v1);
+            CollectionAssert.AreEqual(v2, vettoreOrdinato);
 
         }
     }
--- CollezioneLibrary/Vettore.cs
+++ CollezioneLibrary/Vettore.cs
@@ -8,5 +8,20 @@ namespace CollezioneLibrary
 {
     public class Vettore
     {
+
+        public static int[]Ordina(int[]numeri)
+        {
+            for (int i = 0; i < numeri.Length-1; i++)
+            
+                for (int j = i+1; j < numeri.Length; j++)
+                    if (numeri[i] > numeri[j])
+                    {
+                        int tmp = numeri[i];
+                        numeri[i] = numeri[j];
+                        numeri[j] = tmp;
+                    }
+            
+            return numeri;
+        }
     }
 }
